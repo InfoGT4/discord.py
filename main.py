47f925dcb8889598f69bac6fdee32a479f5a3059
InfoGT4 <41580090+InfoGT4@users.noreply.github.com>
2020-09-06 01:33:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
-token = open("token.txt","r").readline()
+token = open("token.txt","r").readline() # you must have a file called token.txt in the same folder as your .py file
 
 client.run(token)
